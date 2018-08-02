91a4f8a03efab4f0c66c9fa397ad099fb1fecb94
Cristian <creyes.dev@gmail.com>
2018-08-02 01:40:09 -0300

    Unit of work clásico

---
+++ 0_RepasoDotNet/0_UnitOfWorkClasico - copia/Infraestructura/IUnitOfWorkRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructura
+{
+    // Referencia una entidad de negocio que está siendo parte de 
+    // una transacción atómica
+    interface IUnitOfWorkRepository
+    {
+        void PersistirCreacion(IAggregateRoot entity);
+        void PersistirActualizacion(IAggregateRoot entity);
+        void PersistirEliminacion(IAggregateRoot entity);
+    }
+}
---
+++ 0_RepasoDotNet/0_UnitOfWorkClasico - copia/Infraestructura/UnitOfWork.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Transactions;
+
+namespace Infraestructura
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private Dictionary<IAggregateRoot, IUnitOfWorkRepository> entidadesAgregadas;
+        private Dictionary<IAggregateRoot, IUnitOfWorkRepository> entidadesConCambios;
+        private Dictionary<IAggregateRoot, IUnitOfWorkRepository> entidadesEliminadas;
+
+        public UnitOfWork()
+        {
+            entidadesAgregadas = new Dictionary<IAggregateRoot, IUnitOfWorkRepository>();
+            entidadesConCambios = new Dictionary<IAggregateRoot, IUnitOfWorkRepository>();
+            entidadesEliminadas = new Dictionary<IAggregateRoot, IUnitOfWorkRepository>();
+        }
+
+        void RegistrarModificado(IAggregateRoot entity, IUnitOfWorkRepository unitofWorkRepository)
+        {
+            if (!entidadesConCambios.ContainsKey(entity))
+            {
+                entidadesConCambios.Add(entity, unitofWorkRepository);
+            }
+        }
+
+        void RegistrarNuevo(IAggregateRoot entity, IUnitOfWorkRepository unitofWorkRepository)
+        {
+            if (!entidadesAgregadas.ContainsKey(entity))
+            {
+                entidadesAgregadas.Add(entity, unitofWorkRepository);
+            };
+        }
+
+        void RegistrarRemovido(IAggregateRoot entity, IUnitOfWorkRepository unitofWorkRepository)
+        {
+            if (!entidadesEliminadas.ContainsKey(entity))
+            {
+                entidadesEliminadas.Add(entity, unitofWorkRepository);
+            }
+        }
+
+        // Commit de la transacción
+        void Commit()
+        {
+            // Usar la clase TransactionScope que nos asegura efectuar operaciones en 
+            // una transacción atómica
+            using (TransactionScope scope = new TransactionScope())
+            {
+                foreach (IAggregateRoot entity in this.entidadesAgregadas.Keys)
+                {
+                    this.entidadesAgregadas[entity].PersistirCreacion(entity);
+                }
+
+                foreach (IAggregateRoot entity in this.entidadesConCambios.Keys)
+                {
+                    this.entidadesConCambios[entity].PersistirActualizacion(entity);
+                }
+
+                foreach (IAggregateRoot entity in this.entidadesEliminadas.Keys)
+                {
+                    this.entidadesEliminadas[entity].PersistirEliminacion(entity);
+                }
+
+                scope.Complete(); 
+            }
+        }
+
+    }
+}
