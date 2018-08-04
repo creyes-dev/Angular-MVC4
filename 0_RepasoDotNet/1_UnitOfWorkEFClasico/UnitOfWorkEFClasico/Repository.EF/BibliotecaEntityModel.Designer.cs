4492b51bd82b3ddae20e0b76abede8aeda737c87
Cristian <creyes.dev@gmail.com>
2018-08-04 00:15:55 -0300

    Unit of Work Entity Framework

---
+++ 0_RepasoDotNet/1_UnitOfWorkEFClasico/UnitOfWorkEFClasico/Repository.EF/BibliotecaEntityModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\Crist_000\Documents\GitHub\Angular-MVC4\0_RepasoDotNet\1_UnitOfWorkEFClasico\UnitOfWorkEFClasico\Repository.EF\BibliotecaEntityModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
