25729305d68de51d3e53b199419d5d3ce37826d2
creyes-dev <creyes.dev@gmail.com>
2019-01-27 18:36:26 -0300

    Proyecto web api vacío sin mvc
    En VS2012 los proyectos de web api vienen integrados con mvc, se ha procedido a dejar funcionando los módulos fundamentales para los web api

--- 1_WebAPI/0_TareaWebAPI/Tareas/Global.asax.cs
+++ 1_WebAPI/0_TareaWebAPI/Tareas/Global.asax.cs
@@ -19,9 +19,6 @@ namespace Tareas
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
