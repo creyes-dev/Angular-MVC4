1f72c70a89df7df156378dab4e4889a9c6167344
Cristian <creyes.dev@gmail.com>
2019-04-02 21:02:26 -0300

    Nunit interface funcionando con vs2012 5 RC

--- 1_WebAPI/1_JWTAuth/JwtAuth.Tests.Controllers/ValoresControllerTests.cs
+++ 1_WebAPI/1_JWTAuth/JwtAuth.Tests.Controllers/ValoresControllerTests.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Http;
 using JwtAuth.Web.API.Controllers;
 using NUnit.Framework;
 
 namespace JwtAuth.Tests.Controllers
 {
     [TestFixture]
-    public class ValoresControllerGetActionResultTest
+    public class ValoresControllerTests
     {
         ValoresController valoresController;
         string resultado;
@@ -23,7 +24,7 @@ namespace JwtAuth.Tests.Controllers
         }
 
         [Test]
-        public void ObtenerValoresRetornados()
+        public void ValoresControllerObtenerValoresRetornados()
         {
             Assert.IsTrue(resultado == "hola mundo");    
         }
